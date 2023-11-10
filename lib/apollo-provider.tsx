f497d9854216f6dd999dc3f7608649446755f778
Samuel Oluwade <samueloluwade.dev@gmail.com>
2023-11-10 01:52:35 -0600

    Update apollo-provider.tsx

--- lib/apollo-provider.tsx
+++ lib/apollo-provider.tsx
@@ -10,7 +10,7 @@ import {
 
 function makeClient() {
   const httpLink = new HttpLink({
-      uri: process.env.NEXT_PUBLIC_GRAPHQL_API_URL,
+      uri: 'https://main--time-pav6zq.apollographos.net/graphql',
   });
 
   return new NextSSRApolloClient({
