41d34dca8ee154afd1c369cc1f3bd9ee7366f599
Zeus <bamiwade@gmail.com>
2023-11-10 23:54:08 -0600

    Update layout.tsx

--- app/tasklists/layout.tsx
+++ app/tasklists/layout.tsx
@@ -10,20 +10,8 @@ import {RocketIcon} from '@radix-ui/react-icons';
 const session = false;
 
 export default function TasksPageLayout({children}: {children: React.ReactNode}) {
-  // const [client, ssr] = useMemo(() => {
-  //   const ssr = ssrExchange();
-  //   const client = createClient({
-  //     url: process.env.NEXT_PUBLIC_GRAPHQL_API_URL || 'http://localhost:4000/graphql',
-  //     exchanges: [devtoolsExchange, cacheExchange, ssr, fetchExchange],
-  //     suspense: true,
-  //     // requestPolicy: 'cache-and-network',
-  //   });
-
-  //   return [client, ssr];
-  // }, []);
 
   return (
-    // <UrqlProvider client={client} ssr={ssr}>
     <ApolloWrapper>
       {!session && (
         <Alert className='mb-6'>
@@ -34,6 +22,5 @@ export default function TasksPageLayout({children}: {children: React.ReactNode})
       )}
       {children}
     </ApolloWrapper>
-    // </UrqlProvider>
   );
 }
