bd1fb4c9f3076a8ff6cb68306c5c911f15d3e8e4
Zeus <bamiwade@gmail.com>
2023-11-22 07:11:21 -0600

    Update loading.tsx
    darkened loading color

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
   return (
-    <div className='fixed top-0 left-0 w-full h-full flex justify-center items-center bg-slate-800 bg-opacity-90 transition-opacity duration-300 opacity-100'>
+    <div className='fixed top-0 left-0 w-full h-full flex justify-center items-center bg-slate-950 bg-opacity-90 transition-opacity duration-300 opacity-100'>
       <div className='border-8 border-solid border-gray-300 border-t-green-700 rounded-full w-16 h-16 animate-spin'></div>
     </div>
   );
